3aadc2f9631df78b70e6ac2aba6ee1575256fe5d
jeanvalentin51 <valentin.j.gheorghe.com>
2019-10-30 15:04:18 -0400

    Setup the initial variables

--- src/main/java/io/zipcoder/Student.java
+++ src/main/java/io/zipcoder/Student.java
@@ -1,4 +1,68 @@
 package io.zipcoder;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+
 public class Student {
+    private String firstName;
+    private String lastName;
+    private Integer numberOfExamsTaken;
+
+
+
+
+    ArrayList<Double> testScores = new ArrayList<Double>();
+    //ArrayList<Double> examScores;
+
+
+    public Student(String studentFirstName, String studentLasName, Double [] examScores) {
+        this.firstName = studentFirstName;
+        this.lastName = studentLasName;
+        Collections.addAll(this.testScores,examScores);
+        //this.examScores.addAll(Arrays.asList(testScores));
+        //examScores = new ArrayList<Double>();
+    }
+
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public ArrayList<Double> getTestScores() {
+        return testScores;
+    }
+
+    public Integer getNumberOfExamsTaken(){
+        return numberOfExamsTaken;
+    }
+
+
+    public String getExamScores(){
+        String examScores = "";
+
+        for(int i = 0; i < testScores.size(); i++){
+            examScores += formatString(i,testScores.get(i));
+        }
+
+        return examScores;
+    }
+
+    private String formatString (int counter, Double examScore){
+        return "Exam " + (counter+1) + " ->  " + String.format("%.0f",examScore) + "\n";
+    }
+
 }
--- src/test/java/io/zipcoder/StudentTest.java
+++ src/test/java/io/zipcoder/StudentTest.java
@@ -1,5 +1,20 @@
 package io.zipcoder;
 
+import org.junit.Test;
+
 public class StudentTest {
 
+    @Test
+    public void getExamScores() {
+        String fName = "Valentin";
+        String lName = "G";
+        Double [] examScores = {100.0,95.0,85.0,96.0};
+
+        Student  testStudent = new Student(fName,lName,examScores);
+
+        String output = testStudent.getExamScores();
+
+        System.out.println(output);
+
+    }
 }
