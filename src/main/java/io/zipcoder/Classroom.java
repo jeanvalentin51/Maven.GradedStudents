434545efde0e0dc5a741ef749baa22fedc2890f6
jeanvalentin51 <valentin.j.gheorghe.com>
2019-11-02 16:18:15 -0400

    New classes

--- src/main/java/io/zipcoder/Classroom.java
+++ src/main/java/io/zipcoder/Classroom.java
@@ -1,25 +1,5 @@
 package io.zipcoder;
 
-import java.util.Arrays;
-
 public class Classroom {
-    Student [] allStudents;
 
-    public Classroom (Student[] students){
-        this.allStudents = students;
-    }
-
-
-    public Classroom (int maxNumberOfStudents){
-        this.allStudents = new Student[maxNumberOfStudents];
-    }
-
-    public Classroom(){
-        this.allStudents = new Student[30];
-    }
-
-
-    public Student[] getStudents(){
-        return this.allStudents;
-    }
 }
--- src/test/java/io/zipcoder/ClassroomTest.java
+++ src/test/java/io/zipcoder/ClassroomTest.java
@@ -3,15 +3,6 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class ClassroomTest {
-    @Test
-    public void getStudents() {
-        Double [] student1Scores = {100.0,150.0};
-        Double [] student2Scores = {225.0,25.0};
-
-        Student student1 = new Student("student","one",student1Scores);
-        Student student2 = new Student("student","two",student2Scores);
 
 
-
-    }
 }
