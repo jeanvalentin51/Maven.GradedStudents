ddfc6e6bbc91f22fe6b2199afe39cb8bbc69d39e
jeanvalentin51 <valentin.j.gheorghe.com>
2019-11-02 16:37:09 -0400

    Add logic and tests for get students

--- src/main/java/io/zipcoder/Classroom.java
+++ src/main/java/io/zipcoder/Classroom.java
@@ -1,5 +1,38 @@
 package io.zipcoder;
 
-public class Classroom {
+import java.util.Arrays;
 
+public class Classroom {
+    Student [] allStudents;
+
+    public Classroom (Student[] students){
+        this.allStudents = students;
+    }
+
+
+    public Classroom (int maxNumberOfStudents){
+        this.allStudents = new Student[maxNumberOfStudents];
+    }
+
+    public Classroom(){
+        this.allStudents = new Student[30];
+    }
+
+    public Student [] getStudents (){
+        return this.allStudents;
+    }
+
+
+
+
+    public double getAverageExamScore(){
+        double examScores = 0;
+
+        for (int i = 0; i < this.allStudents.length; i++){
+            // examScores = examScores + this.allStudents[i].getTestScores();
+        }
+
+        return examScores/this.allStudents.length;
+    }
 }
+
--- src/test/java/io/zipcoder/ClassroomTest.java
+++ src/test/java/io/zipcoder/ClassroomTest.java
@@ -4,5 +4,43 @@ import org.junit.Test;
 
 public class ClassroomTest {
 
+    @Test
+    public void getStudents() {
+        Double[] student1Scores = {100.0, 150.0, 95.0};
+        Double[] student2Scores = {225.0, 25.0};
+
+        Student student1 = new Student("student", "one", student1Scores);
+        Student student2 = new Student("student", "two", student2Scores);
+
+        Student[] allStudents = {student1, student2};
+        Classroom testClassroom = new Classroom(allStudents);
+
+        Student [] classroomStudents = testClassroom.getStudents();
+        int expected = 3;
+        int actual = classroomStudents[0].getNumberOfExamsTaken();
+
+        Assert.assertEquals(expected,actual);
+
+        expected = 2;
+        actual = classroomStudents[1].getNumberOfExamsTaken();
+
+        Assert.assertEquals(expected,actual);
+    }
+
+
+    @Test
+    public void testGetAverageExamScore() {
+        Double[] student1Scores = {100.0, 150.0};
+        Double[] student2Scores = {225.0, 25.0};
+
+        Student student1 = new Student("student", "one", student1Scores);
+        Student student2 = new Student("student", "two", student2Scores);
+
+        Student[] allStudents = {student1, student2};
+        Classroom testClassroom = new Classroom(allStudents);
+
+    }
+
 
 }
+
